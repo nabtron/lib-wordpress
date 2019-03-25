d70cb6dafad5ee7f89d87c3953b337364e8685a6
nabtron <developer@nabtron.com>
2019-03-25 08:31:38 +0100

    Create lazy-scripts.php

---
+++ edit_plugins_for_speed/custom/lazy-scripts.php
@@ -0,0 +1,28 @@
+<?php defined( 'ABSPATH' ) or die( 'Plugin file cannot be accessed directly.' );
+
+add_filter('script_loader_tag', 'nabcfrs_truefalse', 9999999999, 2);
+
+function nabcfrs_truefalse($tag) {
+    // get list of files
+    $lazy_scripts = ['92a86.js','991e9.js'];
+    
+    return str_replace( ' src', ' data-nabtronjs="true" src', $tag );
+    foreach($lazy_scripts as $lazy_script){
+        if(true == strpos($tag, $lazy_script ) ) {
+            return str_replace( ' src', ' data-nabtronjs="true" src', $tag );
+        }
+    }
+    return $tag;
+}
+
+/*
+add_filter( 'script_loader_tag', 'add_id_to_script', 10, 3 );
+ 
+function add_id_to_script( $tag, $handle, $src ) {
+    if ( 'dropbox.js' === $handle ) {
+        $tag = '<script type="text/javascript" src="' . esc_url( $src ) . '" id="dropboxjs" data-app-key="MY_APP_KEY"></script>';
+    }
+ 
+    return $tag;
+}
+*/
