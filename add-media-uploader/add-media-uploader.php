a872f8191b0cba05ed6603362fc3a48a1006e952
nabtron <developer@nabtron.com>
2019-04-01 10:24:39 +0100

    Create add-media-uploader.php

---
+++ add-media-uploader/add-media-uploader.php
@@ -0,0 +1,28 @@
+<?php
+// this is the only code required, yes only.
+wp_enqueue_script('jquery');
+wp_enqueue_media();
+?>
+<div>
+    <label for="image_url">Image</label>
+    <input type="text" name="image_url" id="image_url" class="regular-text">
+    <input type="button" name="upload-btn" id="upload-btn" class="button-secondary" value="Upload Image">
+
+</div>
+<script type="text/javascript">
+jQuery(document).ready(function($){
+      $('#upload-btn').click(function(e) {
+             e.preventDefault();
+             var image = wp.media({ 
+             title: 'Upload Image',
+             multiple: false
+      }).open()
+      .on('select', function(e){
+     var uploaded_image = image.state().get('selection').first();
+     console.log(uploaded_image);
+     var image_url = uploaded_image.toJSON().url;
+     $('#image_url').val(image_url);
+ });
+});
+});
+</script>
